ca7c949785573e67d8f62494a600794800369cea
Nyan Lin Htun <nyanlinhtun@zawgyimart.com>
2022-08-08 15:08:43 +0630

    first commit

---
+++ app/Filament/Resources/CategoryResource/RelationManagers/PostsRelationManager.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Filament\Resources\CategoryResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Closure;
+use Filament\Forms\Components\Card;
+use Filament\Tables\Columns\TextColumn;
+use Illuminate\Support\Str;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\Toggle;
+use Filament\Tables\Columns\BooleanColumn;
+use Filament\Forms\Components\FileUpload;
+use Filament\Tables\Columns\ImageColumn;
+
+class PostsRelationManager extends RelationManager
+{
+    protected static string $relationship = 'posts';
+
+    protected static ?string $recordTitleAttribute = 'title';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Card::make()->schema([
+                    Select::make('category_id')
+                        ->relationship('category', 'name'),
+                    TextInput::make('title')->reactive()
+                        ->afterStateUpdated(function (Closure $set, $state) {
+                        $set('slug', Str::slug($state));
+                    })
+                    ->required(),
+                    TextInput::make('slug')->required(),
+                    FileUpload::make('image')
+                        ->disk('local')
+                        ->directory('/public/posts'),
+                    RichEditor::make('content'),
+                    Toggle::make('is_published')
+                ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('id')->sortable(),
+                TextColumn::make('title')->limit('50')->sortable(),
+                // TextColumn::make('slug')->limit('50'),
+                BooleanColumn::make('is_published'),
+                // ImageColumn::make('image')->disk('local')
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+}
